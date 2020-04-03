ad061a06edc513ebb571998f69a3c8db897c4c91
Cristiano Augusto Eckel <cristianoaugusto@Mx2.unisc.br>
2020-04-03 13:27:41 -0300

    [ADD] Commit adicionando classe de serviços

--- cityms/cityms/src/main/java/br/com/compasso/cityms/service/CityService.java
+++ cityms/cityms/src/main/java/br/com/compasso/cityms/service/CityService.java
@@ -1,5 +1,8 @@
 package br.com.compasso.cityms.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CityService {
 
 }
---
+++ clientms/clientms/src/main/java/br/com/compasso/clientms/service/ClientService.java
@@ -0,0 +1,5 @@
+package br.com.compasso.clientms.service;
+
+public class ClientService {
+
+}
